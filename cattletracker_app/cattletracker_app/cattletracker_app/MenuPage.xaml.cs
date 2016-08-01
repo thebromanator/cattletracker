14ac22e4dfc3bec7c72620122004fab82b8bc8f5
Daniel <dantheman.uah@gmail.com>
2016-08-01 06:16:48 -0500

    Added placeholder pages for management, sales, myherd, and health.

--- cattletracker_app/cattletracker_app/cattletracker_app/MenuPage.xaml.cs
+++ cattletracker_app/cattletracker_app/cattletracker_app/MenuPage.xaml.cs
@@ -14,5 +14,35 @@ namespace cattletracker_app
         {
             InitializeComponent();
         }
+
+        private void HomeBtn_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new HomePage();
+            IsPresented = false;
+        }
+
+        private void MyHerdBtn_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new MyHerdPage();
+            IsPresented = false;
+        }
+
+        private void ManagementBtn_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new ManagementPage();
+            IsPresented = false;
+        }
+
+        private void HealthBtn_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new HealthPage();
+            IsPresented = false;
+        }
+
+        private void SalesBtn_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new SalesPage();
+            IsPresented = false;
+        }
     }
 }
